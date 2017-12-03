4b7e4bfd8686d2646fcac2b7ef91847a5873d552
xin <xin@snaar.net>
2017-12-03 15:05:53 -0500

    Update Hashing with plp version.

--- Meetup/app/src/main/java/com/example/xin/meetup/database/Hashing.java
+++ Meetup/app/src/main/java/com/example/xin/meetup/database/Hashing.java
@@ -5,20 +5,19 @@ import java.security.NoSuchAlgorithmException;
 
 public class Hashing {
 
-    public static String getHexString(String key) {
+    public static String getHexString(final String key) {
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            final MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(key.getBytes());
-            byte[] byteData = md.digest();
+            final byte[] byteData = md.digest();
 
-            StringBuilder hexString = new StringBuilder();
+            final StringBuilder hexString = new StringBuilder();
             for (int i = 0; i < byteData.length; ++i) {
-                String hex = Integer.toHexString(0xff & byteData[i]);
+                final String hex = Integer.toHexString(0xff & byteData[i]);
                 hexString.append(hex);
             }
             return hexString.toString();
-        }
-        catch (NoSuchAlgorithmException e) {
+        } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
         return null;
