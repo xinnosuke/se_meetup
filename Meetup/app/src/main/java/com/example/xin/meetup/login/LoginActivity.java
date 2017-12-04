d96d4a9ae312bb733c0eb4c86bbd380515623eac
xin <xin@snaar.net>
2017-12-03 23:41:43 -0500

    Fixing missing password error message.

--- Meetup/app/src/main/java/com/example/xin/meetup/login/LoginActivity.java
+++ Meetup/app/src/main/java/com/example/xin/meetup/login/LoginActivity.java
@@ -105,7 +105,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if (!inputValidation.isInputEditTextEmail(textInputEditTextEmail, textInputLayoutEmail, getString(R.string.error_message_email))) {
             valid = false;
         }
-        if (!inputValidation.isInputEditTextFilled(textInputEditTextPassword, textInputLayoutPassword, getString(R.string.error_message_email))) {
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextPassword, textInputLayoutPassword, getString(R.string.error_message_password))) {
             valid = false;
         }
 
