bb070425cc0f528db643e9a9ff90d72c9b6e504b
xin <xin@snaar.net>
2017-12-13 00:04:47 -0500

    Show events on map.

--- Meetup/app/src/main/java/com/example/xin/meetup/map/MapFragment.java
+++ Meetup/app/src/main/java/com/example/xin/meetup/map/MapFragment.java
@@ -2,6 +2,8 @@ package com.example.xin.meetup.map;
 
 import android.Manifest;
 import android.app.Dialog;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
 import com.example.xin.meetup.database.Event;
@@ -9,12 +11,17 @@ import com.example.xin.meetup.util.Constants;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
-public class MapFragment extends SupportMapFragment {
+public class MapFragment extends SupportMapFragment implements OnMapReadyCallback {
     private static final String[] LOCATION_PERMISSIONS = new String[] {
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION,
@@ -25,6 +32,7 @@ public class MapFragment extends SupportMapFragment {
     private int userId;
     private String userType;
     private ArrayList<Event> events;
+    private Address[] addresses;
     private GoogleApiClient gapiClient;
 
 //    private GoogleMap mMap;
@@ -65,23 +73,60 @@ public class MapFragment extends SupportMapFragment {
         userType = args.getString(Constants.USER_TYPE_ARG);
         events = args.getParcelableArrayList(Constants.EVENTS_ARG);
 
-        gapiClient = new GoogleApiClient.Builder(getActivity())
-                .addApi(LocationServices.API)
-                .build();
+//        gapiClient = new GoogleApiClient.Builder(getActivity())
+//                .addApi(LocationServices.API)
+//                .build();
+
+        final Geocoder geocoder = new Geocoder(getContext());
+
+        addresses = new Address[events.size()];
+        for (int i = 0; i < events.size(); ++i) {
+            final Event event = events.get(i);
+            final List<Address> addressList;
+            try {
+                addressList = geocoder.getFromLocationName(event.location, 1);
+            } catch (final IOException e) {
+                e.printStackTrace();
+                continue;
+            }
+            if (!addressList.isEmpty()) {
+                addresses[i] = addressList.get(0);
+            }
+        }
+
+        getMapAsync(this);
     }
 
     @Override
     public void onStart() {
         super.onStart();
 
-        gapiClient.connect();
+//        gapiClient.connect();
     }
 
     @Override
     public void onStop() {
         super.onStop();
 
-        gapiClient.disconnect();
+//        gapiClient.disconnect();
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        for (int i = 0; i < addresses.length; ++i) {
+            final Address address = addresses[i];
+            if (address == null) {
+                continue;
+            }
+
+            final Event event = events.get(i);
+            final LatLng ll = new LatLng(address.getLatitude(), address.getLongitude());
+            final MarkerOptions mo = new MarkerOptions()
+                    .position(ll)
+                    .title(event.name);
+
+            googleMap.addMarker(mo);
+        }
     }
 
     //    @Override
